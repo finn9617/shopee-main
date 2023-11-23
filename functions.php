98f3fd084a60c87c9143bafe0c80244522054149
Nguyennt <nguyennt@bap.jp>
2023-11-23 17:29:55 +0700

    init file

--- functions.php
+++ functions.php
@@ -1,10 +1,41 @@
 <?php 
-
-$response = wp_remote_get( 'https://example.com/api/data' );
-if ( is_array( $response ) ) {
-  $header = $response['headers']; // array of http header lines
-  $body = $response['body']; // use the content
-}
+add_action( 'init-1', "theme_setup");
 
 
+
+function theme_setup(){
+  // PHP Script here
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://shopee.vn/api/v4/flash_sale/get_all_itemids", 
+    CURLOPT_CUSTOMREQUEST => "GET", 
+  ));
+  // $responsePromotionid = curl_exec($curl);
+  // $err = curl_error($curl);
+
+  // curl_close($curl);
+  // if ($err) {
+  //   echo "cURL Error #:" . $err;
+  // } else {
+  //   // print( json_decode($responsePromotionid)[0]);
+    
+  //   // echo gettype( $idColetion);
+  // }
+  // $idColetion = json_decode($responsePromotionid)[0];
+
+  // curl_setopt_array($curl, array(
+  //   CURLOPT_URL => "https://shopee.vn/api/v4/flash_sale/get_all_itemids?need_personalize=true&promotionid=187710547644417&sort_soldout=true&tracker_info_version=0", 
+  //   CURLOPT_CUSTOMREQUEST => "GET", 
+  // ));
+  // $response = curl_exec($curl);
+  // $err = curl_error($curl);
+  // curl_close($curl);
+  // if ($err) {
+  //   echo "cURL Error #:" . $err;
+  // } else {
+  //   echo $response;
+  // }
+
+ }
 ?>
